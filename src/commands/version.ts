a28f7045265b8e08e4d8d8097ed384d9d4536c22
Thomas Carlsen <thomas-carlsen@outlook.com>
2023-08-12 12:32:30 +0200

    shorten createCommand path in version

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,5 +1,5 @@
 import { parse } from "../deps.ts";
-import { createCommand } from "../../src/utils/createCommand.ts";
+import { createCommand } from "../utils/createCommand.ts";
 
 createCommand({
   name: "version",
