7cd3cba3986aee2d9d9cec1d4cf1525c050cab4b
Thomas Carlsen <thomas-carlsen@outlook.com>
2023-08-14 19:09:17 +0200

    simple setup for loading top files

--- main.ts
+++ main.ts
@@ -1,22 +1,12 @@
-import "./src/importCommands.ts";
-import { createCommand, showHelp } from "./src/utils/createCommand.ts";
-
 if (import.meta.main) {
   runMain();
+
 }
 
-function runMain() {
-  const { args } = Deno;
-  const mainCommand = createCommand({
-    name: "main",
-    description:
-      "The dev-cli is made to make life easier - please enjoy life more! ;p",
-    runCommand: async () => {
-      showHelp({ commandName: "main" });
-    },
-    filePath: import.meta.url,
-  });
-  // pick installed args from the parsedArgs
-  // name and version
-  mainCommand.runCommand(args.slice(2, args.length));
+async function runMain() {
+  await loadTopCommands()
+}
+
+async function loadTopCommands() {
+  // find correspting release and load top commands
 }
