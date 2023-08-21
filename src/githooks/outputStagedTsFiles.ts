27c77195ed0703640d4a1c3a7b7b9c5c7c243a21
Thomas Carlsen <thomas-carlsen@outlook.com>
2023-08-21 21:08:57 +0200

    Filter on typescript files on pre-commit linting

--- src/githooks/outputStagedTsFiles.ts
+++ src/githooks/outputStagedTsFiles.ts
@@ -4,6 +4,8 @@ import { writeAllSync } from "../../src/deps.ts";
 const git = simpleGit({ baseDir: "." });
 const s = await git.status();
 const stagedFiles = s.staged;
-const stagedFilesAsString = stagedFiles.join(" ");
+// TODO: should be able to base below extension on argument to this file
+const tsFiles = stagedFiles.filter((f) => f.endsWith(".ts"));
+const stagedFilesAsString = tsFiles.join(" ");
 const contentBytes = new TextEncoder().encode(stagedFilesAsString);
 writeAllSync(Deno.stdout, contentBytes);
